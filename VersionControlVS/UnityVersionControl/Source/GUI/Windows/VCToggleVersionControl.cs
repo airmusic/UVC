10567ae07b51c7e58f286c27eb6a196adf5b1da0
xiaoxiaoyi <gfcocos@gmail.com>
2015-05-23 20:55:07 +0800

    添加快捷键

---
+++ VersionControlVS/UnityVersionControl/Source/GUI/Windows/VCToggleVersionControl.cs
@@ -0,0 +1,18 @@
+﻿namespace VersionControl.UserInterface
+{
+    using UnityEditor;
+    using UnityEngine;
+
+    // ReSharper disable CheckNamespace
+    public class VCToggleVersionControl : Editor
+    // ReSharper restore CheckNamespace
+    {
+        
+        [MenuItem("Window/UVC/Toggle Version Control %h", false, 4)]
+        static void ToggleVersionControl()
+        {
+            VCSettings.VCEnabled = !VCSettings.VCEnabled;
+            Debug.Log(string.Format("Toggle Version Control {0}",VCSettings.VCEnabled?"On":"Off"));
+        }
+    }
+}
